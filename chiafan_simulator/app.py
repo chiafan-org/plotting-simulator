fd7a295cbe925f4c74f24398e33618829b5a2e27
Break Yang <breakds@gmail.com>
2021-05-14 08:53:55 -0700

    Read from sample log

--- chiafan_simulator/app.py
+++ chiafan_simulator/app.py
@@ -4,12 +4,21 @@ import logging
 import re
 from pathlib import Path
 
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+
+from . import templates
+
+
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
 
-
-COMPLETE_PATTERN = re.compile('^(.*INFO.*)plot-k.*\.plot([^\.].*)$')
-
+COMPLETE_PATTERN = re.compile('(.*Renamed final file from.*to.*").*(".*)')
 
 def write_plot(plot_path):
     with open(plot_path, 'w') as f:
@@ -17,21 +26,21 @@ def write_plot(plot_path):
 
 
 @click.command()
-@click.option('--template', default = '',
-              type = click.STRING, help = 'The template log')
+# @click.option('--template', default = '',
+#               type = click.STRING, help = 'The template log')
 @click.option('--destination', default = '',
               type = click.STRING, help = 'Where to put the generated fake plot')
 @click.option('--duration', default = '',
               type = click.FLOAT, help = 'The total simulation duration in seconds')
-def main(template, destination, duration):
-    interval = duration / 2630.0
+def main(destination, duration):
+    interval = duration / 2624.0
     plot_path = Path(destination)
-    with open(Path(template), 'r') as f:
+    with pkg_resources.open_text(templates, 'sample_log.txt') as f:
         for line in f:
             m = COMPLETE_PATTERN.match(line)
             if m is not None:
                 write_plot(plot_path)
-                print(f'{m.groups()[0]}{plot_path.name}{m.groups()[1]}\n')
+                print(f'{m.groups()[0]}{plot_path}{m.groups()[1]}\n')
             else:
                 print(line, end = '')
             time.sleep(interval)
